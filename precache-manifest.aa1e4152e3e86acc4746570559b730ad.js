a043840e37e1cb8683505bb7241983b050bd7043
dwbergstrom <dwbergstrom@gmail.com>
2019-01-17 22:43:53 -0500

    Updates

---
+++ precache-manifest.aa1e4152e3e86acc4746570559b730ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef7e134d212374ed761e",
+    "url": "/transpo-hackathon/static/js/runtime~main.ef7e134d.js"
+  },
+  {
+    "revision": "36e9bb2d6534cb52aff4",
+    "url": "/transpo-hackathon/static/js/main.36e9bb2d.chunk.js"
+  },
+  {
+    "revision": "d79b701dae0e0b9fd501",
+    "url": "/transpo-hackathon/static/js/1.d79b701d.chunk.js"
+  },
+  {
+    "revision": "36e9bb2d6534cb52aff4",
+    "url": "/transpo-hackathon/static/css/main.eeb4be3f.chunk.css"
+  },
+  {
+    "revision": "d35c4759820676f41da08c4bec3f4173",
+    "url": "/transpo-hackathon/index.html"
+  }
+];
